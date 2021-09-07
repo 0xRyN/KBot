5ef31004ea8acc2c48593e45bf76d8bee8fb131d
Rayan <ryanalaz11@gmail.com>
2021-09-07 19:08:00 +0100

    Testing 4

--- app/add-reactions.js
+++ app/add-reactions.js
@@ -1,9 +1,17 @@
-const addReactions = (msg) => {
-    console.log(msg.channelId === "875372171780837406");
+const channels = [
+    "876833649394319412", //Projets
+    "875372225283391489", //NFT
+    "882278309164122172", //Play2Earn
+    "875372204655771678", //Farming
+    "880415002148864081", //Presale
+];
+
+const addReactions = async (msg) => {
     console.log(typeof msg.channelId);
-    if (msg.channelId === "875372171780837406") {
-        console.log("It's in !");
-        msg.react("761601587986432072");
+    if (channels.includes(msg.channelId)) {
+        await msg.react(":fire:");
+        await msg.react(":face_with_raised_eyebrow:");
+        await msg.react(":man_gesturing_no:");
     }
 };
 
