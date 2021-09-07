bcbc56d422659e7567094f2bbf5458d8b5a3f4ea
Rayan <ryanalaz11@gmail.com>
2021-09-07 18:40:51 +0100

    Logging

--- app/add-reactions.js
+++ app/add-reactions.js
@@ -1,4 +1,6 @@
 const addReactions = (msg) => {
+    console.log(msg.channel);
+    console.log(msg.channelId);
     if (msg.channelId === 875372171780837406) {
         msg.react("761601587986432072");
     }
